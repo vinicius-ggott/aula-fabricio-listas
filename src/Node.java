bc47fc00f174c77a6274c9197e30d83c6ecf4245
vinicius-ggott <163208309+vinicius-ggott@users.noreply.github.com>
2025-04-14 02:18:52 -0300

    Update Node.java

--- src/Node.java
+++ src/Node.java
@@ -1,29 +1,26 @@
-// You can extend comparable interface,
-// this enables you to check if two elements are or not equals.
-
 public class Node<T> {
     private T value;
     private Node<T> next;
 
-    public Node(T valor) {
-        this.value = valor;
+    public Node(T value) {
+        this.value = value;
         this.next = null;
-    }
-
-    public Node<T> getNext() {
-        return this.next;
-    }
-
-    public void setNext(Node<T> next) {
-        this.next = next;
     }
 
     public T getValue() {
         return value;
     }
 
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+
     @Override
     public String toString() {
-        return this.value.toString();
+        return value.toString();
     }
 }
